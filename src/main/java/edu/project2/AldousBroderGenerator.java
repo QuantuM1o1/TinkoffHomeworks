fd4fd796921c22932f66e2f96d63f71a26b39e4d
Mikhail Vorobev <mishavorobiev@mail.ru>
2023-11-18 10:52:49 +0600

    Deleted unused import

--- src/main/java/edu/project2/AldousBroderGenerator.java
+++ src/main/java/edu/project2/AldousBroderGenerator.java
@@ -5,7 +5,6 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Random;
 import java.util.Set;
-import java.util.function.Predicate;
 
 public class AldousBroderGenerator implements MazeGenerator {
     @Override
