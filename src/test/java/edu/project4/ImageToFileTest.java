925fd1a98416b4fdf16798f051bdf6d0d9d19881
Mikhail Vorobev <mishavorobiev@mail.ru>
2023-12-10 15:32:43 +0600

    Added all file formats

--- src/test/java/edu/project4/ImageToFileTest.java
+++ src/test/java/edu/project4/ImageToFileTest.java
@@ -10,8 +10,8 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class ImageToFileTest {
     @Test
-    @DisplayName("Проверка создания файла")
-    void fileCreation() {
+    @DisplayName("Проверка создания bmp файла")
+    void bmpFile() {
         // given
         int width = 16;
         int height = 16;
@@ -34,6 +34,68 @@ public class ImageToFileTest {
             symmetry);
         BufferedImage image = ImageDrawer.createImage(pixels, width, height);
         ImageToFile.saveImage(image, path, ImageToFile.ImageFormat.BMP);
+        File answer = new File(path);
+
+        // then
+        assertTrue(answer.exists());
+    }
+
+    @Test
+    @DisplayName("Проверка создания png файла")
+    void pngFile() {
+        // given
+        int width = 16;
+        int height = 16;
+        int iterations = 100;
+        int num = 100;
+        int numberOfAffine = 4;
+        int symmetry = 1;
+        String path = "src/main/resources/project4/flame5.png";
+        FractalFlameRenderer renderer = new MultiThreadRenderer(4);
+        List<NonLinearTransformation> nonLinearTransformations = new ArrayList<>();
+        nonLinearTransformations.add(new SinusoidalTransformation());
+
+
+        // when
+        Pixel[][] pixels = renderer.render(
+            num, iterations,
+            width, height,
+            numberOfAffine,
+            nonLinearTransformations,
+            symmetry);
+        BufferedImage image = ImageDrawer.createImage(pixels, width, height);
+        ImageToFile.saveImage(image, path, ImageToFile.ImageFormat.PNG);
+        File answer = new File(path);
+
+        // then
+        assertTrue(answer.exists());
+    }
+
+    @Test
+    @DisplayName("Проверка создания jpg файла")
+    void jpgFile() {
+        // given
+        int width = 16;
+        int height = 16;
+        int iterations = 100;
+        int num = 100;
+        int numberOfAffine = 4;
+        int symmetry = 1;
+        String path = "src/main/resources/project4/flame6.jpg";
+        FractalFlameRenderer renderer = new MultiThreadRenderer(4);
+        List<NonLinearTransformation> nonLinearTransformations = new ArrayList<>();
+        nonLinearTransformations.add(new SinusoidalTransformation());
+
+
+        // when
+        Pixel[][] pixels = renderer.render(
+            num, iterations,
+            width, height,
+            numberOfAffine,
+            nonLinearTransformations,
+            symmetry);
+        BufferedImage image = ImageDrawer.createImage(pixels, width, height);
+        ImageToFile.saveImage(image, path, ImageToFile.ImageFormat.JPEG);
         File answer = new File(path);
 
         // then
