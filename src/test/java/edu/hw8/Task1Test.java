2d4156bda90d12916b5f7d753518bbb84b14b7c3
Mikhail Vorobev <mishavorobiev@mail.ru>
2023-12-13 15:18:59 +0600

    Deleted unused import

--- src/test/java/edu/hw8/Task1Test.java
+++ src/test/java/edu/hw8/Task1Test.java
@@ -11,7 +11,6 @@ import java.util.stream.Stream;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.assertj.core.api.AssertionsForClassTypes.shouldHaveThrown;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class Task1Test {
